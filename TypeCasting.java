2ba6d0472970ec4683e95d6ff4f44a6785b48278
VaishnaviBiranje <biranjevaishavi2004@gmail.com>
2026-01-22 12:52:28 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,13 @@
+class TypeCasting{
+    public static void main(String args[]){
+        
+        float a =25.12f ;
+        int b = (int)a;
+        System.out.println(""+b);
+
+        char ch1 = 'a';
+        int  ch2 = ch1;
+        System.out.println(""+ch2);
+
+    }
+}
---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+class TypeConversion{
+    public static void main(String arg[]){
+    int a =25;
+    long b = a;
+    System.out.println(b);
+
+    float c= a;
+    System.out.println(c);
+    }
+}
